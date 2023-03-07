e23ccc62962ea97535997ed328e236c494b7410c
Liviu Malaescu <liviu.malaescu@gmail.com>
2023-03-07 20:15:26 +0100

    explicit termination

--- src/folder_scanner/deduplicator.rs
+++ src/folder_scanner/deduplicator.rs
@@ -1,11 +1,20 @@
 use std::{
-    thread::{self, JoinHandle}, sync::{Arc, atomic::AtomicUsize},
+    thread::{self, JoinHandle},
 };
 
 use crossbeam_channel::{unbounded, Receiver, Sender};
 use log::info;
 
+use crate::folder_scanner;
+
 use super::{collector::CollectorResult, file_info::FileInfo};
+
+enum Message {
+    Request { 
+        file_info: Vec<FileInfo>
+    },
+    Terminate
+}
 
 pub struct DeduplicatorResult {
     
@@ -14,7 +23,7 @@ pub struct DeduplicatorResult {
 struct PairDeduplicatorResult {}
 
 pub fn deduplicate(receiver: Receiver<CollectorResult>) -> JoinHandle<()> {
-    let (s, r): (Sender<Vec<FileInfo>>, Receiver<Vec<FileInfo>>) = unbounded();
+    let (s, r): (Sender<Message>, Receiver<Message>) = unbounded();
 
     let worker = thread::spawn(move || {
         info!("Started new thread");
@@ -22,13 +31,18 @@ pub fn deduplicate(receiver: Receiver<CollectorResult>) -> JoinHandle<()> {
             Ok(message) => {
                 let mut children = Vec::new();
                 
-                let counter = fun_name(r, &mut children, message.buckets.len());
+                fun_name(r, &mut children);
 
                 message.buckets.into_iter()
                     .for_each(|dup_pair| {
-                        s.send(dup_pair);
+                        s.send(Message::Request { file_info: dup_pair });
                         ();
                     });
+
+                // signalling worker threads to gracefully close
+                for i in 0..3 {
+                    s.send(Message::Terminate);
+                }
 
                 
                 children.into_iter()
@@ -48,29 +62,29 @@ pub fn deduplicate(receiver: Receiver<CollectorResult>) -> JoinHandle<()> {
     return worker;
 }
 
-fn fun_name(r: Receiver<Vec<FileInfo>>, children: &mut Vec<JoinHandle<()>>, total: usize) -> Arc<AtomicUsize> {
-    let val = Arc::new(AtomicUsize::new(5));
+fn fun_name(r: Receiver<Message>, children: &mut Vec<JoinHandle<()>>) -> () {
     for i in 0..3 {
-        let atomic_counter_c = Arc::clone(&val);
         let receiver = r.clone();
         let worker = thread::spawn(move || {
             let thread_id = thread::current().id();
             info!("{:?} Started new deduplicateur thread", thread_id);
             loop {
                 match receiver.recv() {
-                    Ok(result) => {          
-                        atomic_counter_c.fetch_add(1, std::sync::atomic::Ordering::SeqCst);
-                        info!("{:?} - Processing pair with len {}", thread_id, result.len());
+                    Ok(result) => {
+                        match result {          
+                            Message::Request { file_info } => {
+                                info!("{:?} - Processing pair with len {}", thread_id, file_info.len());
+                            },
+                            Message::Terminate => {
+                                info!("{:?} Quitting", thread_id);
+                                break
+                            },
+                        }
                     },
                     Err(_) => break,
-                }
-                if total == atomic_counter_c.load(std::sync::atomic::Ordering::SeqCst) {
-                    info!("{:?}: Not waiting for new messages", thread_id);
-                    break;
                 }
             }
         });
         children.push(worker);
     };
-    return val;
 }
